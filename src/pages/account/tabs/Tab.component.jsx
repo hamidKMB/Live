5376554c933b4996000dfd2177f4b378bbb8283e
alireza <persian402@gmail.com>
2021-07-26 13:50:41 +0430

    first commit

--- src/pages/account/tabs/Tab.component.jsx
+++ src/pages/account/tabs/Tab.component.jsx
@@ -1,11 +1,17 @@
+//REACT
 import React from "react";
 
+//COMPONENTS
+import PersonalInformation from "./personal-information/personal-information.component";
+
+//MaterialUI
 import AppBar from "@material-ui/core/AppBar";
 import Tab from "@material-ui/core/Tab";
 import TabContext from "@material-ui/lab/TabContext";
 import TabList from "@material-ui/lab/TabList";
 import TabPanel from "@material-ui/lab/TabPanel";
 
+//STYLES
 import "./Tab.styles.scss"
 
 export default function LabTabs() {
@@ -16,8 +22,8 @@ export default function LabTabs() {
     };
 
     return (
-      <div className="tipper">
-        <TabContext value={value}>
+      <div className="tabs">
+        <TabContext value={value} className="tab-context">
             <AppBar position="static">
             <TabList onChange={handleChange} aria-label="simple tabs example" className="tab-list">
                 <Tab label="اطلاعات شخصی" value="1" />
@@ -26,7 +32,7 @@ export default function LabTabs() {
                 <Tab label="تغییر رمز عبور" value="4" />
             </TabList>
           </AppBar>
-          <TabPanel value="1">اطلاعات شخصی</TabPanel>
+          <TabPanel value="1"><PersonalInformation/></TabPanel>
           <TabPanel value="2">تنظیمان</TabPanel>
           <TabPanel value="3">اعلان ها</TabPanel>
           <TabPanel value="4">تغییر رمز عبور</TabPanel>
---
+++ src/pages/account/tabs/personal-information/personal-information.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const PersonalInformation = () => {
+    return (
+        <div>
+            PersonalInformation
+        </div>
+    )
+}
+
+export default PersonalInformation;
