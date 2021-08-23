cd3d4b840bfefd2368b45564b2960217518a3ae2
hamid <hamidkamyab1377@gmail.com>
2021-08-23 12:55:30 +0430

    adding table and design of it for section backup

---
+++ src/components/backup-rows/back-up-rows.component.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+import {ReactComponent as GoodOnProcessClosed} from "./reactions/good-processClosed.svg";
+import {ReactComponent as GoodOnProcess} from "./reactions/good-onprocess.svg";
+import {ReactComponent as Normal} from "./reactions/normal.svg";
+import {ReactComponent as Bad} from "./reactions/bad.svg";
+
+import "./backup-row.styles.scss"
+
+const BackupRow = (props) => {
+    return(
+        <tr className="backup-row d-flex flex-row w-100 mt-2">
+                      <td className="border-0 backup-td">{props.code}</td>
+                      <td className="border-0 backup-td-2">{props.title}</td>
+                      {
+                        !props.isChatOpen &&
+                        <>
+                          <td className="border-0 backup-td">{props.unit}</td>
+                          <td className="border-0 backup-td">{props.date}</td>
+                        </>
+                      }
+                      <td className="border-0 backup-td" onClick={props.onClick}>
+                        {props.situation}
+                      </td>
+                      {
+                        !props.isChatOpen &&
+                          <td className="border-0 backup-td">{
+                              props.situation === "بسته شده" ?
+                              props.reaction === "good" ?
+                              <GoodOnProcessClosed className="good"/>
+                              :
+                              props.reaction === "normal" ?
+                              <Normal className="normal"/>
+                              :
+                              <Bad className="bad"/>
+                              :
+                              <GoodOnProcess/>
+                          }
+                          </td>
+                      }
+                  </tr>
+    )
+}
+
+export default BackupRow
--- src/pages/back-up/back-up.component.jsx
+++ src/pages/back-up/back-up.component.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Modal from "../../components/modal/modal.component";
 import TextInput from "../../components/text-input/text-input.component"
+import BackupRow from "../../components/backup-rows/back-up-rows.component";
 import {ReactComponent as AttachLogo} from "./attach-file.svg"
 import {ReactComponent as BackupAvatar} from "./back-up-avatar.svg"
 import {ReactComponent as CrossLogo} from "./cross-logo.svg"
@@ -12,9 +13,12 @@ import "./backup.styles.scss"
 const BackUp = () => {
   const [isClicked, setIsClicked] = React.useState(false)
   const [userAttachment, setUserAttachment] = React.useState([])
+  const [isChatOpen, setIsChatOpen] = React.useState(false);
+
   const onChangeHandler = (event) => {
      setUserAttachment([...userAttachment, event.target.files[0].name])
   }
+  
   const deleteAttachedFile = (event) => {
     setUserAttachment(
       userAttachment.filter(
@@ -22,11 +26,11 @@ const BackUp = () => {
           item !== event.target.attributes[1].value
       ))
   }
-
+  
   return (
     <div className="admin-pages-layout backup-layout">
       <div className="d-flex flex-row">
-      <div className="d-flex flex-column">
+      <div className={`d-flex flex-column ${isChatOpen ? "w-50" : "w-100"}`}>
         <div className="backup-header d-flex flex-row justify-content-between mb-3 align-items-center w-100">
           <h5 className="mb-0">لیست تیکت ها</h5>
           <div
@@ -92,16 +96,66 @@ const BackUp = () => {
           )}
         </div>
         <table className="table d-flex flex-column">
-          <thead className="thead-back-up">
-            <tr className="card d-flex flex-row">
-              <th scope="col" className="border-0">شماره</th>
-              <th scope="col" className="border-0">عنوان</th>
-              <th scope="col" className="border-0">واحد</th>
-              <th scope="col" className="border-0">تاریخ</th>
-              <th scope="col" className="border-0">وضعیت</th>
-              <th scope="col" className="border-0">بازخورد</th>
+          <thead className="thead-backup">
+            <tr className="card d-flex flex-row justify-content-start">
+              <th scope="col" className="border-0 th-backup">شماره</th>
+              <th scope="col" className="border-0 th-backup-2">عنوان</th>
+              {
+                !isChatOpen &&
+                <>
+                <th scope="col" className="border-0 th-backup">واحد</th>
+                <th scope="col" className="border-0 th-backup">تاریخ</th>
+                </>
+              }
+              <th scope="col" className="border-0 th-backup">وضعیت</th>
+              {
+                !isChatOpen &&
+                <th scope="col" className="border-0 th-backup">بازخورد</th>
+              }
             </tr>
           </thead>
+          <tbody className="tbody-back-up">
+              <BackupRow 
+              code="100235" 
+              title="سشیارذشنسایر" 
+              unit="پشتیبانی" 
+              date="1400/02/25 - 12:30" 
+              situation="درحال پاسخگویی" 
+              isChatOpen={isChatOpen} 
+              onClick={() => setIsChatOpen(!isChatOpen)}/>
+              <BackupRow 
+              code="100235" 
+              title="سشیارذشنسایر" 
+              unit="پشتیبانی" 
+              date="1400/02/25 - 12:30" 
+              situation="درحال بررسی" 
+              isChatOpen={isChatOpen} 
+              onClick={() => setIsChatOpen(!isChatOpen)}/>
+              <BackupRow 
+              code="100235" 
+              title="سشیارذشنسایر" 
+              unit="پشتیبانی" 
+              date="1400/02/25 - 12:30" 
+              situation="بسته شده" 
+              reaction="good" 
+              isChatOpen={isChatOpen} onClick={() => setIsChatOpen(!isChatOpen)}/>
+              <BackupRow 
+              code="100235" 
+              title="سشیارذشنسایر" 
+              unit="پشتیبانی" 
+              date="1400/02/25 - 12:30" 
+              situation="بسته شده" 
+              reaction="normal" 
+              isChatOpen={isChatOpen} onClick={() => setIsChatOpen(!isChatOpen)}/>
+              <BackupRow 
+              code="100235" 
+              title="سشیارذشنسایر" 
+              unit="پشتیبانی" 
+              date="1400/02/25 - 12:30" 
+              situation="بسته شده" 
+              reaction="bad" 
+              isChatOpen={isChatOpen} onClick={() => setIsChatOpen(!isChatOpen)}/>
+          </tbody>
         </table>
       </div>
       </div>
