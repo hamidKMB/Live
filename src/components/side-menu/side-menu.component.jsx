d6d6d4a09b36067321114b9a3984aaad35fc6f27
hamid Kamyab <hamidkamyab1377@gmail.com>
2021-11-19 13:37:09 +0330

    disconnect the header , sideMenu and Dashboard from API

--- src/components/side-menu/side-menu.component.jsx
+++ src/components/side-menu/side-menu.component.jsx
@@ -1,132 +1,129 @@
-import {CameraAlt} from "@material-ui/icons";
+import { CameraAlt } from "@material-ui/icons";
 import SettingsIcon from "@material-ui/icons/Settings";
 // import ViewAgendaOutlinedIcon from "@material-ui/icons/ViewAgendaOutlined";
 // import PermIdentityOutlinedIcon from "@material-ui/icons/PermIdentityOutlined";
 import React from "react";
-import {menuItemsData} from "./menu-items";
+import { menuItemsData } from "./menu-items";
 import MenuItem from "./side-menu-items.component";
-import {useLocation, useHistory} from 'react-router-dom'
-import "./side-menu.styles.scss"
+import { useLocation, useHistory } from "react-router-dom";
+import "./side-menu.styles.scss";
 import MenuMultiActiveItem from "./side-menu-multi-active-items.component";
 import ApiRequest from "../../ApiRequest";
+import userPicture from "./WhatsApp Image 2021-06-22 at 13.15.45.jpeg";
 
 const SideMenu = (props) => {
-    let {pathname} = useLocation();
-    const history = useHistory();
-    const [userProfile, setUserProfile] = React.useState({
-        firstName: null,
-        lastName: null,
-        picture: null ,
-        userName: null ,
-        type: null ,
-        level: null ,
-        mobile: null ,
-        email: null 
-    })
+  let { pathname } = useLocation();
+  const history = useHistory();
+  const [userProfile, setUserProfile] = React.useState({
+    firstName: "حمید",
+    lastName: "کامیاب",
+    picture: "/public/10_upload_video_1.png",
+    userName: "hamidKmb",
+    type: "SuperAdmin",
+    level: "Top",
+    mobile: "09371768426",
+    email: "hamidkamyab1377@gmail.com",
+  });
 
-    React.useEffect(() => {
-        ApiRequest("/user/profile", "GET")
-        .then((res) => {
-            const {fname, lname, image_path, type, level, mobile, email, userName} = res.data ;
-            res.status === "SUCCESS" ?
-            setUserProfile(
-                    {
-                        firstName: fname,
-                        lastName: lname,
-                        picture: image_path ,
-                        userName ,
-                        type ,
-                        level,
-                        mobile,
-                        email,
-                    }
-                )
-            : res.status === "LIMIT_DEVICE" && history.push("/limit_device_list")
-            }
-        )
-        .catch((err) => alert(err))
-    }, [history, setUserProfile])
+  // React.useEffect(() => {
+  //     ApiRequest("/user/profile", "GET")
+  //     .then((res) => {
+  //         const {fname, lname, image_path, type, level, mobile, email, userName} = res.data ;
+  //         res.status === "SUCCESS" ?
+  //         setUserProfile(
+  //                 {
+  //                     firstName: fname,
+  //                     lastName: lname,
+  //                     picture: image_path ,
+  //                     userName ,
+  //                     type ,
+  //                     level,
+  //                     mobile,
+  //                     email,
+  //                 }
+  //             )
+  //         : res.status === "LIMIT_DEVICE" && history.push("/limit_device_list")
+  //         }
+  //     )
+  //     .catch((err) => alert(err))
+  // }, [history, setUserProfile])
 
-    return (
-        <div className={`side-menu ${props.display}`}>
-            <div className="user-profile">
-                <div className="user-profile-holder">
-                    <div className="user-image-holder">
-                        <img src={userProfile.picture} alt="user"/>
-                        <CameraAlt className="camera-icon"/>
-                        <SettingsIcon className="settings-icon"/>
-                    </div>
-                    <div className="user-details">
-                        <h3>{userProfile.firstName + userProfile.lastName}</h3>
-                        <p>{userProfile.mobile}</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="list-of-dashboard">
-                <ul>
-                    {menuItemsData.map((item, index) => {
-                        let pages;
-                        let account_pages = [
-                            '/account/personal-info',
-                            '/account/settings-channel', 
-                            '/account/notifications', 
-                            '/account/change-password'
-                            ];
-                        let videos_pages = [
-                            "/videos/uploaded",
-                            "/videos/courses",
-                            "/videos/bought-videos",
-                            "/videos/liked-videos",
-                            "/videos/upload-video",
-                            "/videos/video-info",
-                            ];
-                        let finance_pages = [
-                            "/finance/wallet",
-                            "/finance/payments",
-                            "/finance/tasfie",
-                            "/finance/sheba",
-                            ];
-                        let followers_pages = [
-                            "/follow/followings",
-                            "/follow/followers"
-                        ]
-                        pages = item.routeName.includes('account') ? account_pages : [];
-                        pages = item.routeName.includes('videos') ? videos_pages : pages;
-                        pages = item.routeName.includes('finance') ? finance_pages : pages;
-                        pages = item.routeName.includes('follow') ? followers_pages : pages;
-                        if (pages.length > 0) {
-                            return (
-                                <MenuMultiActiveItem
-                                    multi_pages={pages}
-                                    pathname={pathname}
-                                    key={index}
-                                    id={index}
-                                    title={item.title}
-                                    routeName={item.routeName}
-                                    logo={item.logo}
-                                    onClick={props.onClick}
-                                />
-                            );
-                        } else {
-                            return (
-                                <MenuItem
-                                    pathname={pathname}
-                                    key={index}
-                                    id={index}
-                                    title={item.title}
-                                    routeName={item.routeName}
-                                    logo={item.logo}
-                                    onClick={props.onClick}
-                                />
-                            );
-                        }
-
-                    })}
-                </ul>
-            </div>
+  return (
+    <div className={`side-menu ${props.display}`}>
+      <div className="user-profile">
+        <div className="user-profile-holder">
+          <div className="user-image-holder">
+            <img src={userPicture} alt="user" />
+            <CameraAlt className="camera-icon" />
+            <SettingsIcon className="settings-icon" />
+          </div>
+          <div className="user-details">
+            <h3>{userProfile.firstName + userProfile.lastName}</h3>
+            <p>{userProfile.mobile}</p>
+          </div>
         </div>
-    );
-}
+      </div>
+
+      <div className="list-of-dashboard">
+        <ul>
+          {menuItemsData.map((item, index) => {
+            let pages;
+            let account_pages = [
+              "/account/personal-info",
+              "/account/settings-channel",
+              "/account/notifications",
+              "/account/change-password",
+            ];
+            let videos_pages = [
+              "/videos/uploaded",
+              "/videos/courses",
+              "/videos/bought-videos",
+              "/videos/liked-videos",
+              "/videos/upload-video",
+              "/videos/video-info",
+            ];
+            let finance_pages = [
+              "/finance/wallet",
+              "/finance/payments",
+              "/finance/tasfie",
+              "/finance/sheba",
+            ];
+            let followers_pages = ["/follow/followings", "/follow/followers"];
+            pages = item.routeName.includes("account") ? account_pages : [];
+            pages = item.routeName.includes("videos") ? videos_pages : pages;
+            pages = item.routeName.includes("finance") ? finance_pages : pages;
+            pages = item.routeName.includes("follow") ? followers_pages : pages;
+            if (pages.length > 0) {
+              return (
+                <MenuMultiActiveItem
+                  multi_pages={pages}
+                  pathname={pathname}
+                  key={index}
+                  id={index}
+                  title={item.title}
+                  routeName={item.routeName}
+                  logo={item.logo}
+                  onClick={props.onClick}
+                />
+              );
+            } else {
+              return (
+                <MenuItem
+                  pathname={pathname}
+                  key={index}
+                  id={index}
+                  title={item.title}
+                  routeName={item.routeName}
+                  logo={item.logo}
+                  onClick={props.onClick}
+                />
+              );
+            }
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default SideMenu;
--- src/pages/dashboard/dashboard.component.jsx
+++ src/pages/dashboard/dashboard.component.jsx
@@ -4,132 +4,133 @@ import React from "react";
 import Details from "../../components/detail/detail";
 
 //STYLES
-import "./dashboard.styles.scss"
-import "../../root-styles/layout.scss"
-import "../../root-styles/buttons.scss"
-import "../../root-styles/cards.scss"
-import "../../root-styles/columns.scss"
+import "./dashboard.styles.scss";
+import "../../root-styles/layout.scss";
+import "../../root-styles/buttons.scss";
+import "../../root-styles/cards.scss";
+import "../../root-styles/columns.scss";
 import ApiRequest from "../../ApiRequest.js";
+
 //DATA
 import Message from "../../components/messages/messages.component";
 import NotificationMessage from "../../components/messages/notifications-message.component";
 import { useHistory } from "react-router-dom";
 
 const Dashboard = () => {
-    const history = useHistory();
-    const [dashboardDetails, setDashboardDetails] = React.useState({
-        abstract: [] ,
-        notifications: [] ,
-        tickets: [],
-        login: [],
-        help: {}
-    })  
-    React.useEffect(() => {
-        ApiRequest("/user/dashboard", "GET")
-        .then((res) => {
-            if (res.status === "SUCCESS") {
-                setDashboardDetails({
-                    abstract: res.data.abstract ,
-                    notifications: res.data.notifications ,
-                    tickets: res.data.tickets ,
-                    login: res.data.login ,
-                    help: res.data.help
-                })
-            } else if (res.status === "LIMIT_DEVICE") history.push("/limit_device_list")
-        })
-        .catch ((err) => console.log(err))
-    },[history])
+  const history = useHistory();
+  const [dashboardDetails, setDashboardDetails] = React.useState({
+    abstract: [],
+    notifications: [],
+    tickets: [],
+    login: [],
+    help: {},
+  });
+  // React.useEffect(() => {
+  //     ApiRequest("/user/dashboard", "GET")
+  //     .then((res) => {
+  //         if (res.status === "SUCCESS") {
+  //             setDashboardDetails({
+  //                 abstract: res.data.abstract ,
+  //                 notifications: res.data.notifications ,
+  //                 tickets: res.data.tickets ,
+  //                 login: res.data.login ,
+  //                 help: res.data.help
+  //             })
+  //         } else if (res.status === "LIMIT_DEVICE") history.push("/limit_device_list")
+  //     })
+  //     .catch ((err) => console.log(err))
+  // },[history])
 
-    return (
-      <div className="dashboard admin-pages-layout">
-        <div className="row">
-          {dashboardDetails.abstract.map((item, index) => (
-            <div className="col-lg-3 col-md-6 col-4 column" key={index}>
-              <div className="card first-row">
-                <h5>
-                  {item.value} {item.unit !== "عدد" && item.unit}
-                </h5>
-                <span>{item.title}</span>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="row">
-          <div className="col-lg-6 col-md-6 column">
-            <div className="card second-row" id="notif">
-              <Details
-                title="اعلان ها"
-                showAll
-                onClick={() => history.push("/account/notifications")}
-              />
-              <div className="message-holder">
-                {dashboardDetails.notifications.map((item, index) => (
-                  <NotificationMessage
-                    notifMessage={
-                      item.title.length > 35
-                        ? item.title.slice(0, 35) + "..."
-                        : item.title
-                    }
-                    dateTime={item.date}
-                    key={index}
-                  />
-                ))}
-              </div>
+  return (
+    <div className="dashboard admin-pages-layout">
+      <div className="row">
+        {dashboardDetails.abstract.map((item, index) => (
+          <div className="col-lg-3 col-md-6 col-4 column" key={index}>
+            <div className="card first-row">
+              <h5>
+                {item.value} {item.unit !== "عدد" && item.unit}
+              </h5>
+              <span>{item.title}</span>
             </div>
           </div>
-          <div className="col-lg-6 col-md-6 column">
-            <div className="card second-row">
-              <Details
-                title="تیکت ها"
-                showAll
-                onClick={() => history.push("/backup")}
-              />
-              <div className="message-holder" id="tickets">
-                {dashboardDetails.tickets.map((item, index) => (
-                  <Message
-                    message={
-                      item.title.length > 35
-                        ? item.title.slice(0, 35) + "..."
-                        : item.title
-                    }
-                    messageDescription={item.status}
-                    ticket={item.status === "بی پاسخ پاسخ" ? true : false}
-                    key={index}
-                  />
-                ))}
-              </div>
+        ))}
+      </div>
+      <div className="row">
+        <div className="col-lg-6 col-md-6 column">
+          <div className="card second-row" id="notif">
+            <Details
+              title="اعلان ها"
+              showAll
+              onClick={() => history.push("/account/notifications")}
+            />
+            <div className="message-holder">
+              {dashboardDetails.notifications.map((item, index) => (
+                <NotificationMessage
+                  notifMessage={
+                    item.title.length > 35
+                      ? item.title.slice(0, 35) + "..."
+                      : item.title
+                  }
+                  dateTime={item.date}
+                  key={index}
+                />
+              ))}
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-lg-6 col-md-6 column">
-            <div className="card">
-              <Details title="ورود و خروج" />
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-3 column" id="wallet">
-            <div className="card">
-              <Details title="کیف پول" />
-              <div className="stock">
-                <span>موجودی حساب :</span>
-                <span>2541000 تومان</span>
-              </div>
-              <div className="button resize">افزایش موجودی</div>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-3 column" id="guide">
-            <div className="card">
-              <Details title="راهنمای سایت" />
-              <div className="guide">
-                <p>{dashboardDetails.help.fullname} عزیز</p>
-                <p>{dashboardDetails.help.description}</p>
-              </div>
-              <div className="button resize px-1">ویدیو آموزش سایت</div>
+        <div className="col-lg-6 col-md-6 column">
+          <div className="card second-row">
+            <Details
+              title="تیکت ها"
+              showAll
+              onClick={() => history.push("/backup")}
+            />
+            <div className="message-holder" id="tickets">
+              {dashboardDetails.tickets.map((item, index) => (
+                <Message
+                  message={
+                    item.title.length > 35
+                      ? item.title.slice(0, 35) + "..."
+                      : item.title
+                  }
+                  messageDescription={item.status}
+                  ticket={item.status === "بی پاسخ پاسخ" ? true : false}
+                  key={index}
+                />
+              ))}
             </div>
           </div>
         </div>
       </div>
-    );
-}
+      <div className="row">
+        <div className="col-lg-6 col-md-6 column">
+          <div className="card">
+            <Details title="ورود و خروج" />
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-3 column" id="wallet">
+          <div className="card">
+            <Details title="کیف پول" />
+            <div className="stock">
+              <span>موجودی حساب :</span>
+              <span>2541000 تومان</span>
+            </div>
+            <div className="button resize">افزایش موجودی</div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-3 column" id="guide">
+          <div className="card">
+            <Details title="راهنمای سایت" />
+            <div className="guide">
+              <p>{dashboardDetails.help.fullname} عزیز</p>
+              <p>{dashboardDetails.help.description}</p>
+            </div>
+            <div className="button resize px-1">ویدیو آموزش سایت</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
